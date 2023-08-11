77b51523936b6309f9ee06c850c125a4140fe8a8
Milan.Bella <Milan.Bella@aomvsr.sk>
2023-08-11 23:33:33 +0200

    m

--- Assets/Scripts/Gaos/Environment/Environment.cs
+++ Assets/Scripts/Gaos/Environment/Environment.cs
@@ -14,14 +14,14 @@ namespace Gaos.Environment
                 env.Add("GAOS_URL", "https://local.galacticodyssey.space/gaos");
                 env.Add("GAOS_WS", "wss://local.galacticodyssey.space/gaos/ws");
                 env.Add("IS_PROFILE_HTTP_CALLS", "true");
-                env.Add("BUNDLES_URL", "https://local.galacticodyssey.space/bundles");
+                env.Add("RELEASE_URL", "https://local.galacticodyssey.space/release");
             }
             else if (envName == "Test")
             {
                 env.Add("GAOS_URL", "https://test.galacticodyssey.space/gaos");
                 env.Add("GAOS_WS", "wss://test.galacticodyssey.space/gaos/ws");
                 env.Add("IS_PROFILE_HTTP_CALLS", "false");
-                env.Add("BUNDLES_URL", "https://test.galacticodyssey.space/bundles");
+                env.Add("RELEASE_URL", "https://test.galacticodyssey.space/release");
 
             }
             else
@@ -35,6 +35,7 @@ namespace Gaos.Environment
 
         public static Dictionary<string, string> GetEnvironment()
         {
+            //return Environment.GetEnvironment_("Development");
             return Environment.GetEnvironment_("Development");
         }
 
