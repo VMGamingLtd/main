62b3a42872c762ece13e327465967a9d7a00863b
Milan.Bella <Milan.Bella@aomvsr.sk>
2023-10-26 22:08:22 +0200

    m

--- Assets/Scripts/Login/RecoverPasswordUsernameScreenManager.cs
+++ Assets/Scripts/Login/RecoverPasswordUsernameScreenManager.cs
@@ -17,6 +17,7 @@ public class RecoverPasswordUsernameScreenManager : MonoBehaviour
     public GameObject infoTextGameObject;
 
     public TMP_InputField passwordOrUsernameTextInput;
+    public TMP_InputField passwordOrUsernameTextInput_placeholder;
 
     public GameObject emailOrUserNameGameObject;
 
@@ -70,7 +71,7 @@ public class RecoverPasswordUsernameScreenManager : MonoBehaviour
     {
         button.interactable = true;
     }
-
+    
     private string GetErrorMessage(Gaos.Routes.Model.UserJson.RecoverPasswordSendVerificationCodeErrorKind errorKind)
     {
         string msg = "";
