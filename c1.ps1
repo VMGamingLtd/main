71cb0a2c76bdfa5ff92492b541598102a5517589
Milan.Bella <Milan.Bella@aomvsr.sk>
2023-11-09 09:56:12 +0100

    m

--- c1.ps1
+++ c1.ps1
@@ -7,8 +7,9 @@
 #invoke update-nginx-on-test-server
 
 #python -m venv .venv
-#.venv\Scripts\Activate.ps1
+#pip install -r requirements.txt
 
+#.venv\Scripts\Activate.ps1
 #invoke drop-gaos-mongo-database-on-test-server
 #invoke release-to-local --platform="webgl" --version="0.0.1" --is-forced
 #invoke publish-to-test-server --platform="webgl" --version="0.0.1", --bundles-version="1" --is-include-build
